18b75341dfe60d09315f70cfdfaa12bbb1c93ff7
Andrew Sowder <Locklandpanther2@yahoo.com>
2022-02-10 10:37:35 -0500

    Merge conflict

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -20,6 +20,7 @@ public class VendingMachineCLI {
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				// display vending machine items
+
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
 			}
